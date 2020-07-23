d5ca646b1bef8065766c8ee929de95e3b6f3d56e
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-23 11:50:35 -0500

    add component files for header, main, wrapper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+
 import './App.css';
 
 function App() {
   return (
-<div>Hello world</div>
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+   Hello world
+
+   </Wrapper>
+    </div>
   );
 }
 

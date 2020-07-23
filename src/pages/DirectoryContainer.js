8e079048098b4a995ea7f08834dfbbf330ddef0d
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-23 17:53:40 -0500

    add state, add componentdidmount, add function to search employees

--- src/pages/DirectoryContainer.js
+++ src/pages/DirectoryContainer.js
@@ -2,38 +2,54 @@ import React, { Component } from "react";
 import Table from "../components/Table";
 import Wrapper from "../components/Wrapper";
 import Navbar from "../components/Navbar";
+import API from "../utils/API";
 
 class DirectoryContainer extends Component {
-//   state = {
-//     currentPage: "Home"
-//   };
+    state = {
+        result: [],
+        search: "",
+        currentPage: ""
+    }
+    componentDidMount() {
+        this.searchEmployee()
+    }
+    searchEmployee = () => {
+        API.getUsers()
+            .then(res => {
+                this.setState({ result: res.data.results })
+            })
+    }
+    // API GET USERS 
+    //   state = {
+    //     currentPage: "Home"
+    //   };
 
-//   handlePageChange = page => {
-//     this.setState({ currentPage: page });
-//   };
+    //   handlePageChange = page => {
+    //     this.setState({ currentPage: page });
+    //   };
 
-//   renderPage = () => {
-//     if (this.state.currentPage === "Home") {
-//       return <Home />;
-//     } else if (this.state.currentPage === "About") {
-//       return <About />;
-//     } else if (this.state.currentPage === "Blog") {
-//       return <Blog />;
-//     } else {
-//       return <Contact />;
-//     }
-//   };
+    //   renderPage = () => {
+    //     if (this.state.currentPage === "Home") {
+    //       return <Home />;
+    //     } else if (this.state.currentPage === "About") {
+    //       return <About />;
+    //     } else if (this.state.currentPage === "Blog") {
+    //       return <Blog />;
+    //     } else {
+    //       return <Contact />;
+    //     }
+    //   };
 
-  render() {
-    return (
-        <main >
-          <Wrapper>
-            <Navbar />
-            <Table />
-          </Wrapper>
-        </main>
-      );
-  }
+    render() {
+        return (
+            <main >
+                <Wrapper>
+                    <Navbar />
+                    <Table />
+                </Wrapper>
+            </main>
+        );
+    }
 }
 
 export default DirectoryContainer;

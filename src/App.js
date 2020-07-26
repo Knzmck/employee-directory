435e2e2841a954f4fe445eb4b908770df20f774e
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-26 10:22:28 -0500

    add exact path and build script

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import DirectoryContainer from "./pages/DirectoryContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const App = () => <DirectoryContainer exact path="/" />;
+function App() {
+    return (
+        <Router>
+            <Route exact path="/" component= { DirectoryContainer }> </Route>
+        </Router>
+    )
+}
+// const App = () => <DirectoryContainer exact path="/" />;
 
 export default App;

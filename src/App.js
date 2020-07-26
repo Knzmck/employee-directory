ecba7da834c9775d1fb7ff07f6eb78a461bbe59d
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-26 10:16:36 -0500

    add exact path and build script

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DirectoryContainer from "./pages/DirectoryContainer";
 
-const App = () => <DirectoryContainer />;
+const App = () => <DirectoryContainer exact path="/" />;
 
 export default App;

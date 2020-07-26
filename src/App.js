5c076820726d6094a8ce9618296869b80aada4c4
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-26 10:38:30 -0500

    add router adn browserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import DirectoryContainer from "./pages/DirectoryContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const App = () => <DirectoryContainer />;
+function App() {
+    return (
+    <Router>
+        <div>
+            <Route exact path="/" component={DirectoryContainer} />
+        </div>
+    </Router> 
+    )
+}
+// const App = () => <DirectoryContainer exact path="/" />;
 
 export default App;

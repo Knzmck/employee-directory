a8fb755c05cc2883c1a5fb36af86c0e64f117acb
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-26 10:26:24 -0500

    dklajf

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import DirectoryContainer from "./pages/DirectoryContainer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-    return (
-        <Router>
-            <Route exact path="/" component= { DirectoryContainer }> </Route>
-        </Router>
-    )
-}
-// const App = () => <DirectoryContainer exact path="/" />;
+const App = () => <DirectoryContainer />;
 
 export default App;
